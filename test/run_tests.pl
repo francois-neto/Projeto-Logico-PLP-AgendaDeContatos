6bc12feba32aa4bab36dbaee6f0d1fe0c6c77197
Davi-Falcao <davifaf3106@gmail.com>
2026-07-28 22:37:05 -0300

    test: inclui suite de integracao na execucao dos testes

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -1,6 +1,7 @@
-:- consult('test/csv_utils_test.pl').
-:- consult('test/contato_db_test.pl').
-:- consult('test/contato_repository_test.pl').
+:- consult('integration_test.pl').
+:- consult('csv_utils_test.pl').
+:- consult('contato_db_test.pl').
+:- consult('contato_repository_test.pl').
 
 run_tests :-
     plunit:run_tests.
